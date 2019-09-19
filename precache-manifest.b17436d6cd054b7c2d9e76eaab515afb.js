fce50b42b1c050c021d1f272f39c507e62e38d01
Alexandre Victoor <alexandre.victoor@fluo.com>
2019-09-19 18:06:06 +0200

    Updates

---
+++ precache-manifest.b17436d6cd054b7c2d9e76eaab515afb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c712ae380414081e4d12642878080a4",
+    "url": "/tic-tac-toe-rl/index.html"
+  },
+  {
+    "revision": "dc840e67de7698fc2259",
+    "url": "/tic-tac-toe-rl/static/css/main.48cfa216.chunk.css"
+  },
+  {
+    "revision": "19faaa139525e2e52e5d",
+    "url": "/tic-tac-toe-rl/static/js/2.df19ded4.chunk.js"
+  },
+  {
+    "revision": "dc840e67de7698fc2259",
+    "url": "/tic-tac-toe-rl/static/js/main.b20fdc32.chunk.js"
+  },
+  {
+    "revision": "44cd05e9f50825325497",
+    "url": "/tic-tac-toe-rl/static/js/runtime~main.20cd0e75.js"
+  }
+]);
